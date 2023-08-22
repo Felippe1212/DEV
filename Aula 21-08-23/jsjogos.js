fdc85ea3e34c0b3e44e82131d758fd4d887e094a
Felippe Guimaraes Gabriel <felippe.g.prog@gmail.com>
2023-08-22 12:07:27 -0300

    Exercicios

---
+++ Aula 21-08-23/jsjogos.js
@@ -0,0 +1,9 @@
+let pontuaçaoI=500;
+let pontosE=100;
+let perda=5;
+let final=pontuaçaoI+pontosE-perda;
+console.log ("Sua pontuação inicial é de:",pontuaçaoI);
+console.log("Você ganhou pontos extras de:",pontosE);
+console.log("Seus pontos perdidos foram de:",perda);
+console.log("Sua pontuaçao final ficou no total de:",final);
+
